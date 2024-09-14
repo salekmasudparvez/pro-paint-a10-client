2b4591d6f449510d1471e88507b3a17996932965
salekmasudparvez <salekmasudparvez@gmail.com>
2024-09-14 20:38:21 +0600

    use env

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBP9hxJTfYTibozhxJUPjGm37lpTpD47n8",
-  authDomain: "pro-painting.firebaseapp.com",
-  projectId: "pro-painting",
-  storageBucket: "pro-painting.appspot.com",
-  messagingSenderId: "664918939476",
-  appId: "1:664918939476:web:225a94a3a2f61c8e3cdac1"
-};
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
